ea64a22ce9ea9938f8f9862799c8f0ac191d86cc
Ethan Sylvester <ethan.sylvester@gmx.com>
2022-05-27 17:15:27 -0400

    Updates

---
+++ precache-manifest.cbbcd220e3cacc2b569f4393be1832f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c83d629ca837539d13ec",
+    "url": "/businesswebsite/static/css/main.6a8a79a1.chunk.css"
+  },
+  {
+    "revision": "c83d629ca837539d13ec",
+    "url": "/businesswebsite/static/js/main.c83d629c.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/businesswebsite/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "3f311bc8244c8ba89280",
+    "url": "/businesswebsite/static/js/2.3f311bc8.chunk.js"
+  },
+  {
+    "revision": "e7e0e94d523f16a206a3",
+    "url": "/businesswebsite/static/js/runtime~main.e7e0e94d.js"
+  },
+  {
+    "revision": "47d598e5fa3589e7be14f39572cd5d04",
+    "url": "/businesswebsite/static/media/profile.47d598e5.jpg"
+  },
+  {
+    "revision": "d15f278c2fd8696e954b52672d34c5ec",
+    "url": "/businesswebsite/static/media/instagram.d15f278c.png"
+  },
+  {
+    "revision": "09e99ac154ec4688ea55f8ca6fa2a960",
+    "url": "/businesswebsite/static/media/profile.09e99ac1.jpg"
+  },
+  {
+    "revision": "4d8ed02ff592838f5ebe55f44468ab3a",
+    "url": "/businesswebsite/static/media/action.4d8ed02f.jpg"
+  },
+  {
+    "revision": "8a312b30a5fb7e1fc59c06a84df0a75f",
+    "url": "/businesswebsite/static/media/ZenLoop.8a312b30.ttf"
+  },
+  {
+    "revision": "160fef9bc7bb1fabe2f7f93b275a5ce9",
+    "url": "/businesswebsite/index.html"
+  }
+];
